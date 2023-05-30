fb4b5c791d059894848addbebbe17dc7b288f84d
Giovanna Simione <giovannasimionero@gmail.com>
2023-05-30 15:28:05 -0400

    segunda alteração

--- divisaao/Program.cs
+++ divisaao/Program.cs
@@ -1,7 +1,16 @@
-﻿Console.WriteLine("Informe o primeiro valor: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor: ");
-int b = int.Parse(Console.ReadLine());
+﻿using System;
 
-int result = a / b;
-Console.WriteLine(result);
+try
+{
+    Console.WriteLine("Informe o primeiro valor: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor: ");
+    int b = int.Parse(Console.ReadLine());
+
+    int result = a / b;
+    Console.WriteLine(result);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Não é possível dividir qualquer número por zero");
+}
