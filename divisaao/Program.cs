01d53e31cd9575db32ce19f8d8ea78e256ccaaee
Giovanna Simione <giovannasimionero@gmail.com>
2023-05-30 15:02:14 -0400

    Inicio do projeto de divisao de dois numeros

---
+++ divisaao/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Informe o primeiro valor: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor: ");
+int b = int.Parse(Console.ReadLine());
+
+int result = a / b;
+Console.WriteLine(result);
